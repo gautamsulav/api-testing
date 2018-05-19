881df03b3188951e6205ec9596f79e60c3325a93
sulav <gs.sulav@gmail.com>
2018-05-18 22:51:58 -0500

    updated with only two files remaining

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ $json_data = json_decode($json,true);
 
 <html>
 <head>
-  <title>API Testing</title>
-  <meta charset="utf-8">
+	<title>API Testing</title>
+	<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -60,49 +60,21 @@ $json_data = json_decode($json,true);
   </table>
   </div>
 </div>
-  
+	
 
 
 </body>
 </html>
 
 <script type="text/javascript">
-  var i=0;
-  setInterval(function(){
-    $.ajax({ 
-      type: 'GET', 
-      url: 'http://skuninja.madeby.ws/logs.json', 
-      dataType: 'json',
-      success: function (data) {
-        $('#master').empty();
-          $.each(data, function(index, value) {
-            $.each(value,function(i,v){
-              $.each(v,function(k,val){
-                ++i;
-                var background;
-                if(val.type==1)
-                {
-                  background='Green';
-                }else if(val.type==2)
-                {
-                  background='Yellow';
-                }
-                else
-                {
-                  background='#B80000';
-                }
-                $('#myTable tbody').append('<tr class="child" bgcolor='+background + '><td><font color="white">'+i+'</font></td><td><font color="white">'+val.created+'</font></td><td><font color="white">'+val.subject+'</font></td></tr>');
-              
-              });
-              
-            });
+	$(document).ready(function() {
+	  setInterval(function() {
+	    cache_clear()
+	  }, 30000);
+	});
 
-          });
-          
-      }
-  });
-
-  },2000);
-
-
+	function cache_clear() {
+	  window.location.reload(true);
+	  // window.location.reload(); use this if you do not remove cache
+	}
 </script>
